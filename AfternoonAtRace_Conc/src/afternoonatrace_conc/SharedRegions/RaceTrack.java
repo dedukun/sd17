6e0b2fa80a2f7ac68a580eb3f31db4b9495e5505
Filipe <filipereis96@ua.pt>
2018-03-13 18:25:35 +0000

    Start , project structure.

---
+++ AfternoonAtRace_Conc/src/afternoonatrace_conc/SharedRegions/RaceTrack.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package afternoonatrace_conc.SharedRegions;
+
+/**
+ *
+ * @author Filipe
+ */
+public class RaceTrack {
+    
+}
