b19cda87edaf292bea4d74f2f9c24461bd0bc6e1
xdye <dierduarte@gmail.com>
2018-05-06 22:29:20 +0100

    Continue with script

--- TP2/compile.sh
+++ TP2/compile.sh
@@ -1,20 +1,56 @@
 #!/bin/bash
 
-# Get the main class and build directory from the project properties
-properties="./nbproject/project.properties"
-mainClassProperty=$(cat $properties | grep "main.class=")
-mainClass="${mainClassProperty/main.class=/}"
+clear
 
-# Compile source
-if [ ! -f "./src/$(echo $mainClass | tr . /).java" ]; then
-  echo "The main class ${mainClass} wasn't found"
-  exit 1
-fi
+function build {
+    folder=$1
 
-# Run clean
-if [ "$1" == "clean" ]; then
-  eval "ant -q -f $(pwd) -Dnb.internal.action.name=rebuild clean jar"
-fi
+    # Get the main class and build directory from the project properties
+    properties="./$folder/nbproject/project.properties"
+    mainClassProperty=$(cat $properties | grep "main.class=")
+    mainClass="${mainClassProperty/main.class=/}"
 
-# run program
-eval "ant -q -f $(pwd) -Dnb.internal.action.name=run run"
+    # Compile source
+    if [ ! -f "./$folder/src/$(echo $mainClass | tr . /).java" ]; then
+      echo "The main class ${mainClass} wasn't found"
+      exit 1
+    fi
+
+    # Run clean
+    if [ "$1" == "clean" ]; then
+      eval "ant -q -f $(pwd)/$folder -Dnb.internal.action.name=rebuild clean jar"
+    fi
+}
+
+
+servers=( "BettingCenterServer" "ControlCenterServer" "GenReposServer" "PaddockServer"
+"RaceTrackServer" "StableServer" )
+
+printf "Building Servers...\n"
+
+for server in "${servers[@]}"
+do
+
+    printf "  "$server"..."
+
+    build $server
+
+    echo "done"
+
+    # run program
+    #eval "ant -q -f $(pwd)/$server -Dnb.internal.action.name=run run"
+done
+
+clients=( "BrokerClient" "HorseJockeyClient" "SpectatorClient" )
+
+printf "\nBuilding Clients...\n"
+
+for client in "${clients[@]}"
+do
+
+    printf "  "$client"..."
+
+    build $client
+
+    echo "done"
+done
