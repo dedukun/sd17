e71fc3df1e859116b635b76f0a476ea18c3bc95f
xdye <dierduarte@gmail.com>
2018-05-14 20:54:10 +0100

    Deleted duplicated BettingCenter

--- BettingCenter/src/Auxiliar/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Package with all the auxiliar files used.<br>
- *  - All entities states<br>
- *  - Configurations<br>
- *  - Simulation Parameters
- */
-package Auxiliar;
--- BettingCenter/src/Stubs/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Package with all the stubs used in this computational device.<br>
- *  All server stubs.
- */
-package Stubs;
