61c667f1aace53095215d2c06088b682778421c7
Filipe <filipereis96@ua.pt>
2018-06-07 10:05:20 +0100

    BrokerClient changes

--- TP3/AfternoonAtRaces_RMI/src/clientSide/Broker/BrokerClient.java
+++ TP3/AfternoonAtRaces_RMI/src/clientSide/Broker/BrokerClient.java
@@ -9,6 +9,8 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import registry.RegistryConfiguration;
 
 public class BrokerClient {
@@ -26,57 +28,23 @@ public class BrokerClient {
         ControlCenterInterface controlCenter = null;
         RaceTrackInterface raceTrack = null;
         StableInterface stable = null;
-
+        Registry registry = null;
+        
         //Modificar isto para ir buscar parametetros ao ficheiro de confguração
         String rmiRegHostName = RegistryConfiguration.REGISTRY_RMI_HOST;
         int rmiRegPortNumb = RegistryConfiguration.REGISTRY_RMI_PORT;
 
+        try{
+            registry = LocateRegistry.getRegistry(rmiRegHostName, rmiRegPortNumb);
+        } catch (RemoteException ex) {
+            Logger.getLogger(BrokerClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
         //Vai buscar interface do Betting Center
         try {
-               Registry registry = LocateRegistry.getRegistry(rmiRegHostName, rmiRegPortNumb);
                bettingCenter = (BettingCenterInterface) registry.lookup(RegistryConfiguration.REGISTRY_BETTING_CENTER);
-           } catch (RemoteException e) {
-               System.out.println("Exception finding logger: " + e.getMessage() + "!");
-               e.printStackTrace();
-               System.exit(1);
-           } catch (NotBoundException e) {
-               System.out.println("Logger is not registed: " + e.getMessage() + "!");
-               e.printStackTrace();
-               System.exit(1);
-        }
-
-        //Vai buscar interface do Control Center
-        try {
-               Registry registry = LocateRegistry.getRegistry(rmiRegHostName, rmiRegPortNumb);
                controlCenter = (ControlCenterInterface) registry.lookup(RegistryConfiguration.REGISTRY_CONTROL_CENTER);
-           } catch (RemoteException e) {
-               System.out.println("Exception finding logger: " + e.getMessage() + "!");
-               e.printStackTrace();
-               System.exit(1);
-           } catch (NotBoundException e) {
-               System.out.println("Logger is not registed: " + e.getMessage() + "!");
-               e.printStackTrace();
-               System.exit(1);
-        }
-
-        //Vai buscar interface do  Stable
-        try {
-               Registry registry = LocateRegistry.getRegistry(rmiRegHostName, rmiRegPortNumb);
                stable = (StableInterface) registry.lookup(RegistryConfiguration.REGISTRY_STABLE);
-           } catch (RemoteException e) {
-               System.out.println("Exception finding logger: " + e.getMessage() + "!");
-               e.printStackTrace();
-               System.exit(1);
-           } catch (NotBoundException e) {
-               System.out.println("Logger is not registed: " + e.getMessage() + "!");
-               e.printStackTrace();
-               System.exit(1);
-        }
-
-        //Vai buscar interface do Race Track
-        try {
-               Registry registry = LocateRegistry.getRegistry(rmiRegHostName, rmiRegPortNumb);
-               raceTrack = (RaceTrackInterface) registry.lookup(RegistryConfiguration.REGISTRY_RACE_TRACK);
+               
            } catch (RemoteException e) {
                System.out.println("Exception finding logger: " + e.getMessage() + "!");
                e.printStackTrace();
